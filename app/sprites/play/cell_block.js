bc70a6a7b1014ec0b236e75fdd7727083dfd7cfd
Jeff Revesz <jeffrey.revesz@gmail.com>
2016-03-18 19:37:50 -0400

    resolves #12

--- app/sprites/play/cell_block.js
+++ app/sprites/play/cell_block.js
@@ -4,21 +4,20 @@ import Cell from "./cell";
 
 class CellBlock extends Phaser.Group {
 
-  constructor(game, grid, max_size, color) {
-    super(game);
-    this.grid = grid;
+  constructor(game, cell_block_group, starting_cell, max_size, color) {
+    super(game, cell_block_group);
+    this.starting_cell = starting_cell;
+    this.x = starting_cell.x;
+    this.y = starting_cell.y;
     this.max_size = max_size;
-    this.cells = [];
     this.color = color;
+    this.cells = [];
     this.add_cells();
+    this.group_cells();
   }
 
   add_cells() {
-    let cell = _.sample(this.grid.empty_cells);
-
-    if (!cell) {
-      return;
-    }
+    let cell = this.starting_cell;
 
     while (this.cells.length < this.max_size) {
       this.add_cell(cell);
@@ -37,6 +36,23 @@ class CellBlock extends Phaser.Group {
     cell.color = this.color;
     cell.add_squircle();
     this.cells.push(cell);
+  }
+
+  group_cells() {
+    // set the pos of this group
+    this.x = _.minBy(this.cells, "x").x;
+    this.y = _.minBy(this.cells, "y").y;
+    console.log("cell block:", this);
+
+    // overwrite the pos of each cell
+    // with the Group-relative value
+    // then add it to the Group
+    let cell_block = this;
+    _.forEach(this.cells, function(cell) {
+      cell.x = cell.x - cell_block.x;
+      cell.y = cell.y - cell_block.y;
+      cell_block.add(cell);
+    });
   }
 }
 
--- app/sprites/play/cell_block_group.js
+++ app/sprites/play/cell_block_group.js
@@ -6,19 +6,26 @@ class CellBlockGroup extends Phaser.Group {
 
   constructor(game, grid, max_size,
       max_cells_per_cell_block, color=Math.random() * 0xFFFFFF) {
-    super(game);
+    super(game, grid);
     this.grid = grid;
     this.max_size = max_size;
     this.max_cells_per_cell_block= max_cells_per_cell_block;
     this.color = color;
-    this.cell_blocks = [];
     this.add_cell_blocks();
   }
 
   add_cell_blocks() {
-    while(this.grid.empty_cells.length > 0 && this.cell_blocks.length < this.max_size) {
-      this.cell_blocks.push(new CellBlock(this.game, this.grid,
-          this.max_cells_per_cell_block, this.color));
+    while(this.grid.empty_cells.length > 0 &&
+        this.children.length < this.max_size) {
+
+      let block_starting_cell = _.sample(this.grid.empty_cells);
+      if (!block_starting_cell) {
+        return;
+      }
+
+      let cb = new CellBlock(this.game, this,
+          block_starting_cell, this.max_cells_per_cell_block, this.color);
+      this.add(cb);
     }
   }
 }
